269706f67e9d0ddd81641a6691cae5a53921995b
rentka <107320686+rentkaa@users.noreply.github.com>
2022-06-11 22:27:48 -0500

    Multiplayer ready
    Ver: 2.0

--- Source/titan/Character/Hero.cpp
+++ Source/titan/Character/Hero.cpp
@@ -5,6 +5,7 @@
 #include "GameFramework/SpringArmComponent.h"
 #include "Camera/CameraComponent.h"
 #include "GameFramework/CharacterMovementComponent.h"
+#include "Components/WidgetComponent.h"
 // Sets default values
 AHero::AHero()
 {
@@ -21,6 +22,9 @@ AHero::AHero()
 
 	bUseControllerRotationYaw = false;
 	GetCharacterMovement()->bOrientRotationToMovement = true;
+	OverheadWidget = CreateDefaultSubobject<UWidgetComponent>(TEXT("OverheadWidget"));
+	OverheadWidget->SetupAttachment(RootComponent);
+
 }
 
 
